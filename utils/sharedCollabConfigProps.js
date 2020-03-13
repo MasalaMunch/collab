3d99fd8b4a3a7c2d239033ad45ba18e07b81712c
MasalaMunch <rsdean@wisc.edu>
2020-03-13 18:01:10 -0500

    see previous commit

---
+++ utils/sharedCollabConfigProps.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = new Set([
+    "IntentAsChanges", 
+    "updateDerivedState",
+    "defaultVal",
+    "KeyAsString",
+    "KeyFromString",
+    "ValAsString",
+    "ValFromString",
+    "IntentAsString",
+    "IntentFromString",
+    ]);
