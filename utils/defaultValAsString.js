ad881afad066306ab188564f87b6c72cae80ea12
MasalaMunch <rsdean@wisc.edu>
2020-03-19 14:07:08 -0500

    added type assertion for defaultValAsString

--- utils/defaultValAsString.js
+++ utils/defaultValAsString.js
@@ -1,6 +1,9 @@
 "use strict";
 
-const JsoAsString = require(`./JsoAsString.js`);
+const assert = require(`./assert.js`);
 const defaultVal = require(`./defaultVal.js`);
-
+const JsoAsString = require(`./JsoAsString.js`);
+    
 module.exports = JsoAsString(defaultVal);
+
+assert(typeof module.exports ==== `string`);
